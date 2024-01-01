1ab1aeeab2246a6df8a8f0a2c095456c95016945
iifeofadarkrose <122921193+iifeofadarkrose@users.noreply.github.com>
2024-01-01 23:42:10 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
