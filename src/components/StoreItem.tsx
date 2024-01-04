2ac78dab72343195eb2d37a558bf040fc7966c5b
iifeofadarkrose <122921193+iifeofadarkrose@users.noreply.github.com>
2024-01-04 14:33:27 +0100

    context Hooks

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,4 @@
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -10,6 +11,14 @@ type StoreItemProps = {
 const quantity = 1;
 
 function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
   return (
     <div className="h-[100%]">
       <img src={imgUrl} alt="" className="object-cover h-52 w-full" />
@@ -20,24 +29,29 @@ function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         </h1>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <button className="text-white w-full rounded-md p-2 bg-gray-500 border-collapse border-2 border-solid border-fuchsia-700">
+            <button onClick={()=> increaseCartQuantity(id)} className="text-white w-full rounded-md p-2 bg-gray-500 border-collapse border-2 border-solid border-fuchsia-700">
               +Add to cart
             </button>
           ) : (
             <div className="flex items-center justify-center flex-col gap-1">
               <div className="flex flex-row justify-center items-center gap-2">
-                 <button className="text-white rounded-md p-3 bg-gray-500 border-collapse border-2 border-solid border-fuchsia-700">-</button>
-              <div>
-                <span className="text-[30px]">{quantity}</span>
-                In Cart
+                <button onClick={()=> decreaseCartQuantity(id)} className="text-white rounded-md p-3 bg-gray-500 border-collapse border-2 border-solid border-fuchsia-700">
+                  -
+                </button>
+                <div>
+                  <span className="text-[30px]">{quantity}</span>
+                  In Cart
+                </div>
+
+                <button onClick={()=> increaseCartQuantity(id)} className="text-white rounded-md p-3 bg-gray-500 border-collapse border-2 border-solid border-fuchsia-700">
+                  +
+                </button>
               </div>
 
-              <button className="text-white rounded-md p-3 bg-gray-500 border-collapse border-2 border-solid border-fuchsia-700">+</button>
-              </div>
-             
-              
               <div className="flex items-center justify-center gap-1">
-                <button className="fs-4 rounded-md p-2 bg-red-500 border-collapse border-2 border-solid">Remove</button>
+                <button onClick={()=> removeFromCart(id)} className="fs-4 rounded-md p-2 bg-red-500 border-collapse border-2 border-solid">
+                  Remove
+                </button>
               </div>
             </div>
           )}
